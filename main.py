fb1ae3494a8aa639368dbf6e60469336a7fa3266
gangan <gangan.education@gmail.com>
2022-07-03 03:20:10 +0900

    fix: 画像に文字を描画する

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+# -*- coding: utf-8 -*-
+
 import argparse
 from datetime import datetime as dt
-from PIL import Image, ImageDraw
-from typing import Any
+from PIL import Image, ImageDraw, ImageFont
 
 
-def draw_text(text:str="2022年07月01 19:00 ～ 21:00") -> Any:
+def draw_text(date:str="2022年07月01 19:00 ～ 21:00") -> None:
     """画像に日時を描画する
 
     Args:
-        text (str): 描画日時. Defaults to "2022年07月01 19:00 ～ 21:00".
-        
-    Returns:
-        Any: Pillow.Image.Imageを返すかもしれない
+        date (str): 描画日時. Defaults to "2022年07月01 19:00 ～ 21:00".
     """
+    img = Image.new("RGB", (1024, 384), (256, 256, 256))
+    draw = ImageDraw.Draw(img)
+    font_type = ["./font/HannariMincho-Regular.otf"]
+    color = [(0, 0, 0), (256, 256, 256)]
+    text = [
+        "これはテストです",
+        "【開催日時】",
+        "テストの文章を生成してみよう。\n何も考えられないので結構適当な文章です。"
+    ]
+
+    # 描画する
+    draw.text((512, 64), text[0], fill=color[0], anchor="mm",
+            font=ImageFont.truetype(font_type[0], 60))
+    draw.rectangle([(30, 140),(994, 220)], fill=(255, 0, 0))
+    draw.text((512, 180), text[1] + date, fill=color[1], anchor="mm",
+            font=ImageFont.truetype(font_type[0], 32))
+    draw.text((512, 300), text[2], fill=color[0], anchor="mm",
+            font=ImageFont.truetype(font_type[0], 32))
+
+    # 画像を保存する
+    img.save("./output/test.png")
 
 
 def convertDate(start:str, stop:str) -> str:
