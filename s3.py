66943920cedb8506b7f69a1d3cb4118998036298
gangan <gangan.education@gmail.com>
2022-07-04 23:28:35 +0900

    fix return type

--- s3.py
+++ s3.py
@@ -5,7 +5,7 @@ from boto3.session import Session
 from typing import Any
 
 def upload_s3(
-    client: Any,
+    session: boto3.Session,
     bucketName: str = "testBucket",
     bucketDir: str = "/tttttt",
     fileName: str = "test.txt"
@@ -13,12 +13,12 @@ def upload_s3(
     """S3にファイルをアップロードする
 
     Args:
-        client (Any): 一時クレデンシャル発行済みclient
+        session (boto3.Session): AssumeROle取得済みSession情報.
         bucketName (str): アップロードするS3のバケット名. Defaults to "testBucket".
         bucketDir (str): アップロードするS3のフォルダ名. Defaults to "/tttttt".
         fileName (str): アップロードするファイル名. Defaults to "test.txt".
     """
-    s3 = client.resource("s3")
+    s3 = session.resource("s3")
     bucket = s3.Bucket(bucketName)
     bucket.upload_file(fileName, bucketDir + "/" + fileName)
 
@@ -26,12 +26,14 @@ def upload_s3(
 def get_assume_role(
     roleArn: str = "arn:aws:iam:: XXXXX:role/role_name",
     roleSessionName: str = "test"
-) -> Any:
+) -> boto3.Session:
     """AssumeRoleを取得する
 
     Args:
         roleArn (str): Role Arn. Defaults to "arn:aws:iam:: XXXXX:role/role_name".
         roleSessionName (str): Role Session Name. Defaults to "test".
+    Returns:
+        AssumeRole Session.
     """
     client = boto3.client("sts")
 
@@ -51,9 +53,10 @@ def get_assume_role(
         aws_session_token=response["Credentials"]["SessionToken"],
         region_namee="ap-northeast-1"
     )
-
+    
+    # テスト
     client = session.client("sts")
     account_id = client.get_caller_identity()["Account"]
     print("Account ID: {0}".format(account_id))
 
-    return client
+    return session
